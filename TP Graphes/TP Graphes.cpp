077f2a0d8a5b3e5a41497e830d898f2a1e973887
thcornu <thcornu@A210PC06>
2020-09-22 12:00:06 +0200

    fin de séance

--- TP Graphes/TP Graphes.cpp
+++ TP Graphes/TP Graphes.cpp
@@ -2,6 +2,7 @@
 //
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "Graph.h"
 
 
@@ -10,10 +11,12 @@ int main()
 	graphe_t mon_graphe;
 	solution_t ma_solution;
 	int ordre[NBMAX_SOMMETS] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
 	std::ifstream data("DLP_210.dat");
-
+	std::string str;
 	if (data) {
-
+		std::getline(data, str);
+		std::cout << str << std::endl;
 	}
 
 	/*initGraphe(mon_graphe);
